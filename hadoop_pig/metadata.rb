bb8c9d1ffeb3a1c92d87f9ef2ddc95583aacf620
Stefhen Hovland <stefhen@rightscale.com>
2012-07-10 15:24:33 -0700

    added hadoop_pig cookbook

---
+++ hadoop_pig/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "Rightscale Inc"
+maintainer_email "support@rightscale.com"
+license          "All rights reserved"
+description      "Installs/Configures Pig for Hadoop"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe "hadoop_pig::install", "Installs Pig"
