5ba0fbffa32f3850b2a5b6c440827cc001233f3f
Stefhen Hovland <stefhen@rightscale.com>
2012-07-10 15:10:25 -0700

    metadata

---
+++ hadoop_pig/attributes/default.rb
@@ -0,0 +1,3 @@
+set['pig']['version'] = '0.10.0'
+set['pig']['install_dir'] = '/home/hadoop'
+set['pig']['source'] = 
--- hadoop_pig/metadata.rb
+++ hadoop_pig/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Rightscale Inc"
+maintainer_email "support@rightscale.com"
 license          "All rights reserved"
-description      "Installs/Configures hadoop_pig"
+description      "Installs/Configures Pig for Hadoop"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+recipe "hadoop_pig::install", "Installs Pig"
