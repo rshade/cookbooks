6b163d7d5c5efb1a18b7e3948de4705bcbb2d9b2
Stefhen Hovland <stefhen@rightscale.com>
2012-08-29 15:24:47 -0400

    copy phpunit files from cookbook to /tmp

--- astral/recipes/default.rb
+++ astral/recipes/default.rb
@@ -1,7 +1,8 @@
 
-# Install compass and dependencies
 
 rightscale_marker :begin
+
+# Install compass and dependencies
 
 %w[compass chunky_png oily_png compass-rgbapng].each do |p|
   gem_package "#{p}" do
@@ -9,5 +10,25 @@ rightscale_marker :begin
     gem_binary "/usr/bin/gem"
   end
 end
+
+phpunit_packages = %w[
+  File_Iterator-1.3.1.tgz 
+  Text_Template-1.1.1.tgz 
+  PHP_Timer-1.0.2.tgz 
+  YAML-1.0.6.tgz 
+  PHP_TokenStream-1.1.3.tgz 
+  PHP_CodeCoverage-1.1.3.tgz 
+  PHPUnit_MockObject-1.1.1.tgz 
+  PHP_Invoker-1.1.0.tgz
+]
+
+# Copy phpunit packages from cookbook to /tmp
+phpunit_packages.each do |file|
+  cookbook_file "/tmp/#{file}" do
+    source "#{file}"
+  end
+end
+
+
 
 rightscale_marker :end
