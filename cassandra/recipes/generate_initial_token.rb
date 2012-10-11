a8ce304c6665366ef418dd9f03dfece73706a48e
Stefhen Hovland <stefhen@rightscale.com>
2012-10-11 13:29:47 -0400

    dink

--- cassandra/recipes/generate_initial_token.rb
+++ cassandra/recipes/generate_initial_token.rb
@@ -10,8 +10,8 @@
 rightscale_marker :begin
 
 ruby "generate_initial_token" do
-#  node.set['cassandra']['initial_token'] = `/usr/bin/token-generator -n #{node['cassandra']['node_total']}`.to_a.grep(/Node #0?#{node['cassandra']['node_number']}:/).first.split.last
-  node.set['cassandra']['initial_token'] = "this_is_the_initial_token"
+  #node.set['cassandra']['initial_token'] = `/usr/bin/token-generator -n #{node['cassandra']['node_total']}`.to_a.grep(/Node #0?#{node['cassandra']['node_number']}:/).first.split.last
+  node.set['cassandra']['initial_token'] = %x[/usr/bin/token-generator -n #{node['cassandra']['node_total']}`.to_a.grep(/Node #0?#{node['cassandra']['node_number']}:/].first.split.last
 end
 
 rightscale_marker :end
