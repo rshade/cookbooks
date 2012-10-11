29afef37a2fab5be590a67ce401d2367fce30191
Stefhen Hovland <stefhen@rightscale.com>
2012-10-10 22:54:24 -0400

    xxx

--- cassandra/recipes/default.rb
+++ cassandra/recipes/default.rb
@@ -9,9 +9,9 @@
 
 rightscale_marker :begin
 
-include_recipe "cassandra::install_cassandra"
-include_recipe "cassandra::generate_initial_token"
-include_recipe "cassandra::install_config_files"
-include_recipe "cassandra::set_tags"
+#include_recipe "cassandra::install_cassandra"
+#include_recipe "cassandra::generate_initial_token"
+#include_recipe "cassandra::install_config_files"
+#include_recipe "cassandra::set_tags"
 
 rightscale_marker :end
