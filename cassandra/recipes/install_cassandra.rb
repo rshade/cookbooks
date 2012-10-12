992cc503e11b787c7a82cea7f2b7123d32cad9a3
rshade <rshade@rightscale.com>
2012-10-12 11:36:44 -0500

    Update cassandra/recipes/install_cassandra.rb

--- cassandra/recipes/install_cassandra.rb
+++ cassandra/recipes/install_cassandra.rb
@@ -14,6 +14,8 @@ bash "install_cassandra" do
   code <<-EOM
     tar xf apache-cassandra-1.1.5-bin.tar.gz -C /usr/local
   EOM
+  notifies :create, "ruby_block[generate_initial_token]", :immediately
+  not_if "test -e /tmp/initial_token-#{node['rightscale']['instance_uuid']}"
 end
 
 log "NODE_NUMBER: #{node['cassandra']['node_number']}"
@@ -25,6 +27,7 @@ ruby_block "generate_initial_token" do
     file.write(%x[/usr/local/apache-cassandra-1.1.5/tools/bin/token-generator -n #{node['cassandra']['node_total']}].to_a.grep(/Node #0?#{node['cassandra']['node_number']}:/).first.split.last)
     file.close
   end
+  action :nothing
 end
 
 rightscale_marker :end
