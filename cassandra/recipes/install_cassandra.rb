bd0356cb71f6fcaf25118265afc428e42ebc4ee7
Stefhen Hovland <stefhen@rightscale.com>
2012-10-12 00:45:12 -0400

    xxx

--- cassandra/recipes/install_cassandra.rb
+++ cassandra/recipes/install_cassandra.rb
@@ -12,20 +12,24 @@ rightscale_marker :begin
 case node['platform']
 when "ubuntu"
   include_recipe "apt"
-  apt_repository "cassandra-repo" do
+  a = apt_repository "cassandra-repo" do
     uri "http://www.apache.org/dist/cassandra/debian"
     components ["11x", "main"]
     keyserver "pgp.mit.edu"
     key "4BD736A82B5C1B00"
-    action :add
+    action :nothing
     notifies :run, resources(:execute => "apt-get update"), :immediately
   end
+  a.run_action(:add)
 end
 
 # Installing the cassandra debian package also installs openjdk.
 # Oracle java is already installed at /usr/lib/jvm/java-6-sun and should be used
 # for $JAVA_HOME instead.
 
-package "cassandra"
+p = package "cassandra" do
+  action :nothing
+end
+p.run_action(:install)
 
 rightscale_marker :end
